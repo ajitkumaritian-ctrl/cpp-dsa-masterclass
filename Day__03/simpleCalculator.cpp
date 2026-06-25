9690130a89ef3542aeaf26207145eda33e4ebcbb
AjitKumarDev <ajitkumar.itian@gmail.com>
2026-06-25 20:29:12 +0530

    Update simpleCalculator.cpp

--- Day__03/simpleCalculator.cpp
+++ Day__03/simpleCalculator.cpp
@@ -3,5 +3,13 @@ using namespace std;
 int main(){
   // Q1: 
   // Take two numbers and print addition, subtraction, multiplication and division.
-  
+  int num1, num2;
+  cout << "Enter num1 : ";
+  cin >> num1;
+  cout << "Enter num2 : ";
+  cin >> num2;
+  cout << "Addition = " << num1 + num2 << endl;
+  cout << "Subtraction = " << num1 - num2 << endl;
+  cout << "Multiplication = " << num1 * num2 << endl;
+  cout << "Division = " << num1 / num2 << endl;
 }
