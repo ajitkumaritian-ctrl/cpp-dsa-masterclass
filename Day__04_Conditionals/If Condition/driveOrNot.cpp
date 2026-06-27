2af9d0266c862f7a924e0e9645aa757e5cd1b517
AjitKumarDev <ajitkumar.itian@gmail.com>
2026-06-27 08:48:43 +0530

    Conditionals

---
+++ Day__04_Conditionals/If Condition/driveOrNot.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // Take age as input and tell if person can drive or not
+  int age;
+  cout << "Enter your age: ";
+  cin >> age;
+
+  if(age >= 18){
+    cout << "You can drive.";
+  }
+  if (age < 18){
+    cout << " You can not drive.";
+  }
+}
+
+// IF condition ---> me condition hr baar check krna padta hai,,,, upar waala galat ho ya shi ,,,, no matter
