9dfe66e972aa40fc846b545d385acfbc151bbcbb
AjitKumarDev <ajitkumar.itian@gmail.com>
2026-06-27 10:18:04 +0530

    Create oddOrEven.cpp

---
+++ Day__04_Conditionals/If Condition/oddOrEven.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //Take positive integer input and tell if it is odd or even
+  int n;
+  cout << "Enter the number: ";
+  cin >> n;
+
+  if(n%2==0){
+    cout << "Even number." << endl;
+  }
+  if (n%2==1){
+    cout << "Odd number." << endl;
+  }
+
+  //WE ARE DEALING ONLY USING IF ...
+}
